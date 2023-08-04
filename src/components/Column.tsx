45cfbf456dae87b1b47e00ae41dfb5efbaa3b2f6
lukesrw <lukesrw+github@gmail.com>
2023-08-04 13:58:13 +0100

    Added twMerge, resolves #2

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 export interface ColumnProps extends PropsWithChildren {
     title: string;
@@ -7,7 +8,7 @@ export interface ColumnProps extends PropsWithChildren {
 
 export function Column(props: ColumnProps) {
     return (
-        <div className={`flex-1 flex flex-col mb-4 xl:mb-8 ${props.className}`}>
+        <div className={twMerge("flex-1 flex flex-col mb-4 xl:mb-8", props.className)}>
             <h1 className="text-3xl mb-4 font-extrabold">{props.title}</h1>
 
             {props.children}
