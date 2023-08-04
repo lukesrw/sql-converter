8a4d4df5740b7b3efcea05833cec0a123a6764b0
lukesrw <lukesrw+github@gmail.com>
2023-08-04 20:11:50 +0100

    Added Analytics component, resolves #4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
@@ -8,21 +9,17 @@ export const metadata: Metadata = {
     title: "SQL Converter",
     description: "Easily convert between SQL, JS, and PHP",
     icons: {
-        icon: "/favicon.png",
-    },
+        icon: "/favicon.png"
+    }
 };
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body
-                className={`${font.className} bg-zinc-950 text-zinc-200 h-screen`}
-            >
+            <body className={`${font.className} bg-zinc-950 text-zinc-200 h-screen`}>
                 {children}
+
+                <Analytics></Analytics>
             </body>
         </html>
     );
