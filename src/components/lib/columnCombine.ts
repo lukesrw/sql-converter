0db3d8163f6923fe9b69e2822ba4c0428e8201b6
lukesrw <lukesrw+github@gmail.com>
2023-08-05 22:26:17 +0100

    Removed unused libs

--- src/components/lib/columnCombine.ts
+++
@@ -1,8 +0,0 @@
-import { escapeWrap } from "./escapeWrap";
-
-export function columnCombine(...parts: string[]) {
-    return parts
-        .filter(Boolean)
-        .map((part) => escapeWrap(part, "`"))
-        .join(".");
-}
