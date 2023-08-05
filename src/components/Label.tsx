a155cf5a74d81ae608220f0d4060a240251bdd29
lukesrw <lukesrw+github@gmail.com>
2023-08-05 21:30:46 +0100

    Added twMerge with className prop

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,14 +1,15 @@
-import { PropsWithChildren } from "react";
+import { LabelHTMLAttributes, PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
-export const INPUT_STYLE = "bg-white/10 p-4 py-3 block mt-2 rounded-xl font-normal border-4 border-white/20";
+export const INPUT_STYLE = "bg-white/10 p-4 py-3 block mt-2 rounded-xl font-normal border-4 border-white/20 w-full";
 
-export interface LabelProps extends PropsWithChildren {
+export interface LabelProps extends PropsWithChildren, Pick<LabelHTMLAttributes<HTMLLabelElement>, "className"> {
     label: string;
 }
 
 export function Label(props: LabelProps) {
     return (
-        <label className="text-sm font-semibold mb-4 block">
+        <label className={twMerge("text-sm font-semibold mb-4 block", props.className)}>
             {props.label}
             {props.children}
         </label>
