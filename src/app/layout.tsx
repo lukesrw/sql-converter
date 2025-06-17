9478f3765cbfbb0570b8a3739bcabaed08e0368c
lukesrw <lukesrw+github@gmail.com>
2025-06-17 20:20:34 +0100

    Add suppression

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import "./globals.css";
 
 const font = Poppins({ subsets: ["latin"], weight: "400" });
 
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`${font.className} bg-zinc-950 text-zinc-200 h-screen`}>
+            <body className={`${font.className} bg-zinc-950 text-zinc-200 h-screen`} suppressHydrationWarning>
                 {children}
 
                 <Analytics></Analytics>
