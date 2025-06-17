387d30d6ed1f3ef9c397ba7b25303d06da8e6ed2
lukesrw <lukesrw+github@gmail.com>
2025-06-17 21:23:52 +0100

    Increase font size

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -9,7 +9,7 @@ export namespace Column {
 export function Column(props: Readonly<Column.Props>) {
     return (
         <div className="flex-1 flex flex-col pb-8 xl:pb-0 xl:mb-4">
-            <h1 className="text-3xl mb-4 font-extrabold">{props.title}</h1>
+            <h1 className="text-4xl mb-4 font-extrabold">{props.title}</h1>
 
             {props.children}
         </div>
