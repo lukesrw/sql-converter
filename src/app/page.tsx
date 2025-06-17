07d6e8aee35cef258f69fee7fc8dd6b6fb9d6d84
lukesrw <lukesrw+github@gmail.com>
2025-06-17 21:18:59 +0100

    Improve context, improve betweens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,71 +4,13 @@ import { Column } from "@/components/Column";
 import { JSEditor } from "@/components/Editor/JSEditor";
 import { PHPEditor } from "@/components/Editor/PHPEditor";
 import { SQLEditor } from "@/components/Editor/SQLEditor";
-import { QueryContext, Variables } from "@/hooks/useQueryContext";
-import { useEffect, useState } from "react";
-
-const QUERY_KEY = "SQLConverter_Query";
-const VARIABLES_KEY = "SQLConverter_Variables";
+import { QueryContextProvider } from "@/hooks/useQueryContext";
 
 export default function Home() {
-    const [query, setQuery] = useState("");
-    const [variables, setVariables] = useState<Variables>({});
-
-    /**
-     * When the page first mounts, check localStorage for query/variable history
-     * If we find but aren't able to parse the variables we reset the history
-     */
-    useEffect(() => {
-        let storedQuery = localStorage.getItem(QUERY_KEY);
-        let storedVariables: any = localStorage.getItem(VARIABLES_KEY);
-        if (storedVariables) {
-            try {
-                storedVariables = JSON.parse(storedVariables);
-            } catch (_1) {
-                storedQuery = null;
-                storedVariables = null;
-            }
-        }
-
-        setQuery(
-            storedQuery ||
-                `SELECT
-    *
-FROM
-    People
-WHERE
-    People.Forename = 'Frank' AND
-    People.Surname = @Surname AND
-    People.Age = @Age`
-        );
-
-        setVariables(
-            storedVariables || {
-                Surname: "Skinner",
-                Age: "66"
-            }
-        );
-    }, []);
-
-    /**
-     * Update localStorage whenever the query/variables change
-     */
-    useEffect(() => {
-        localStorage.setItem(QUERY_KEY, query);
-        localStorage.setItem(VARIABLES_KEY, JSON.stringify(variables));
-    }, [query, variables]);
-
     return (
         <>
             <main className="p-4 pb-0 gap-4 xl:p-6 xl:pb-0 xl:gap-6 h-full grid grid-cols-1 xl:grid-cols-3">
-                <QueryContext.Provider
-                    value={{
-                        query,
-                        setQuery,
-                        variables,
-                        setVariables
-                    }}
-                >
+                <QueryContextProvider>
                     <Column title="SQL">
                         <SQLEditor />
                     </Column>
@@ -78,7 +20,7 @@ WHERE
                     <Column title="PHP">
                         <PHPEditor />
                     </Column>
-                </QueryContext.Provider>
+                </QueryContextProvider>
             </main>
         </>
     );
--- src/hooks/useQueryContext.tsx
+++ src/hooks/useQueryContext.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, createContext, useContext } from "react";
+import { Dispatch, PropsWithChildren, SetStateAction, createContext, useContext, useEffect, useState } from "react";
 
 export type Variables = Record<string, string>;
 
@@ -8,6 +8,82 @@ export const QueryContext = createContext<{
     variables: Variables;
     setVariables: Dispatch<SetStateAction<Variables>>;
 } | null>(null);
+
+const QUERY_KEY = "SQLConverter_Query";
+const VARIABLES_KEY = "SQLConverter_Variables";
+
+export function QueryContextProvider(props: Readonly<PropsWithChildren>) {
+    const [query, setQuery] = useState("");
+    const [variables, setVariables] = useState<Variables>({});
+
+    /**
+     * When the page first mounts, check localStorage for query/variable history
+     * If we find but aren't able to parse the variables we reset the history
+     */
+    useEffect(() => {
+        let storedQuery = localStorage.getItem(QUERY_KEY);
+        let storedVariables: any = localStorage.getItem(VARIABLES_KEY);
+        if (storedVariables) {
+            try {
+                storedVariables = JSON.parse(storedVariables);
+            } catch (_1) {
+                storedQuery = null;
+                storedVariables = null;
+            }
+        }
+
+        setQuery(
+            storedQuery ||
+                `SELECT
+    *
+FROM
+    People
+WHERE
+    People.Forename = 'Frank' AND
+    People.Surname = @Surname AND
+    People.Age = @Age`
+        );
+
+        setVariables(
+            storedVariables || {
+                Surname: "Skinner",
+                Age: "66"
+            }
+        );
+    }, []);
+
+    useEffect(() => {
+        /**
+         * Replace betweens, not supported by formatter.
+         */
+        if (!query.toLowerCase().includes(" between ")) {
+            return;
+        }
+
+        setQuery(query.replace(/(\s*)(.+) BETWEEN (.+) AND (.+)/gi, "$1($2 >= $3 AND $2 <= $4)"));
+    }, [query]);
+
+    /**
+     * Update localStorage whenever the query/variables change
+     */
+    useEffect(() => {
+        localStorage.setItem(QUERY_KEY, query);
+        localStorage.setItem(VARIABLES_KEY, JSON.stringify(variables));
+    }, [query, variables]);
+
+    return (
+        <QueryContext.Provider
+            value={{
+                query,
+                setQuery,
+                variables,
+                setVariables
+            }}
+        >
+            {props.children}
+        </QueryContext.Provider>
+    );
+}
 
 export function useQueryContext() {
     const context = useContext(QueryContext);
