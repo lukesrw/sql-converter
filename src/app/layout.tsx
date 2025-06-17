9d69299dc5631ee06fa301f87b62b457897a97e2
lukesrw <lukesrw+github@gmail.com>
2025-06-17 21:21:00 +0100

    Updated font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import "./globals.css";
-
-const font = Poppins({ subsets: ["latin"], weight: "400", preload: true, display: "swap" });
 
 export const metadata: Metadata = {
     title: "SQL Converter",
@@ -16,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`${font.className} bg-zinc-950 text-zinc-200 h-screen`} suppressHydrationWarning>
+            <body className="bg-zinc-950 text-zinc-200 h-screen font-mono" suppressHydrationWarning>
                 {children}
 
                 <Analytics></Analytics>
